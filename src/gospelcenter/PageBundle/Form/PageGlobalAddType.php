68039c8739fbc4c90d0412242adc271312adeeb1
Yoann Gern <webyg@icloud.com>
2014-02-22 18:56:54 +0100

    Fixed bug about Page Type

--- src/gospelcenter/PageBundle/Form/PageGlobalAddType.php
+++ src/gospelcenter/PageBundle/Form/PageGlobalAddType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class PageAddType extends PageType
+class PageGlobalAddType extends PageGlobalType
 {
     
     /**
