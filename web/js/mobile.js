90ae4f800772de3ddcdb4b1460daf7cad30fbd95
Yoann Gern <webyg@icloud.com>
2014-04-01 22:44:56 +0200

    bug fixed

--- web/js/mobile.js
+++ web/js/mobile.js
@@ -1,4 +1,4 @@
-// @codekit-prepend "vendor/jquery-1.10.2.js.js"
+// @codekit-prepend "vendor/jquery-1.10.2.js"
 // @codekit-prepend "vendor/jquery.touchSwipe.js"
 // @codekit-prepend "vendor/modernizr.js"
 // @codekit-append "vendor/handlebars-v1.3.0.js"
