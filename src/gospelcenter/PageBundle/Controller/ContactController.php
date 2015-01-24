814f20b68b149b816b76ed601033a3cb7bb421dc
yoanngern <yoann@yoanngern.ch>
2015-01-24 20:57:25 +0100

    contact message

---
+++ src/gospelcenter/PageBundle/Controller/ContactController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace gospelcenter\PageBundle\Controller;
+
+use gospelcenter\CenterBundle\Entity\Center;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ContactController extends Controller
+{
+
+
+    /**
+     * @param Center $center
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function contactAction(Center $center)
+    {
+
+        $request = $this->get('request');
+
+        if ($request->getMethod() == 'POST') {
+            $name = $request->request->get('name');
+            $email = $request->request->get('email');
+            $phone = $request->request->get('phone');
+            $txt = $request->request->get('message');
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('www.gc-'. $center->getRef() .'.org - Formulaire de contact')
+                ->setFrom($email)
+                ->setTo('web_contact@gc-'. $center->getRef() .'.org')
+                ->setBody(
+                    $this->renderView(
+                        'gospelcenterPageBundle:Mail:contact.html.twig',
+                        array(
+                            'center' => $center,
+                            'title' => 'Formulaire de contact',
+                            'name' => $name,
+                            'email' => $email,
+                            'phone' => $phone,
+                            'message' => $txt
+                        )
+                    ),
+                    'text/html'
+                );
+            $this->get('mailer')->send($message);
+
+            return $this->render(
+                'gospelcenterPageBundle:Mail:thanks.html.twig',
+                array(
+                    'center' => $center,
+                    'page' => 'contact'
+                )
+            );
+        }
+
+        return $this->redirect(
+            $this->generateUrl(
+                'gospelcenterContact',
+                array(
+                    'center' => $center->getRef()
+                )
+            )
+        );
+    }
+
+    public function contactTestAction(Center $center)
+    {
+
+        return $this->render(
+            'gospelcenterPageBundle:Mail:contact.html.twig',
+            array(
+                'center' => $center,
+                'title' => 'Formulaire de contact',
+                'name' => 'Yoann',
+                'email' => 'yoann@yoanngern.ch',
+                'phone' => '077 444 19 01',
+                'message' => 'Hello! Every body'
+            )
+        );
+    }
+
+}
